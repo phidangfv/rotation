0b919edc0141a9ab20aeb8c9b9c48a2adea64810
dangngocphi <dangngocphi@gspit.net>
2023-10-23 14:26:10 +0700

    feat: opt image bg

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,14 +1,15 @@
-import image1 from "./iphone.png";
-import image2 from "./speaker.png";
-import image3 from "./tv.png";
-import image4 from "./laptop.png";
-import image5 from "./car.png";
-import image6 from "./wash.png";
-import image7 from "./coin.png";
-import image8 from "./trip.png";
-import image9 from "./amazon.png";
 import image10 from "./airpod.png";
-import background from "./background.jpg";
+import image9 from "./amazon.png";
+import image5 from "./car.png";
+import image7 from "./coin.png";
+import image1 from "./iphone.png";
+import image4 from "./laptop.png";
+import image2 from "./speaker.png";
+import image8 from "./trip.png";
+import image3 from "./tv.png";
+import image6 from "./wash.png";
+// import background from "./background.jpg";
+import background from "./background-de-thuong.jpg";
 
 const IMAGES = {
   image1,
